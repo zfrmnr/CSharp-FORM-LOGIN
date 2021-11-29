88fac7f2dde3b5683ee92d94ed89375deef6f481
zfrmnr <zfrmnr@gmail.com>
2021-11-29 16:50:49 +0300

    Add project files.

---
+++ deneme2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace deneme2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\90537\Documents\Deneme2.mdf;Integrated Security=True;Connect Timeout=30;");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From login Where Username='"+ textBox1.Text +"' And Password='"+ textBox2.Text +"'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            if(dt.Rows[0][0].ToString() == "1") { 
+
+            this.Hide();
+            Home ss = new Home();
+            ss.Show();
+                        }
+            else
+            {
+
+                MessageBox.Show("please check information");
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
